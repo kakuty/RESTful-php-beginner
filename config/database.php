9f4fb6fa05b048e32938d02c146dcd39c2da4148
Lam Nguyen <im2lazzzzzy@gmail.com>
2017-07-30 08:45:55 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,25 +1,26 @@
 <?php
-    class Database {
-      private $host = 'localhost';
-      private $username = 'root';
-      private $password = '';
-      private $dbname='api_db';
+class Database
+{
+    private $host = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $dbname='api_db';
 
-      public $conn;
+    public $conn;
 
-      // Get the database connection
-      public function getConnection() {
+  // Get the database connection
+    public function getConnection()
+    {
         $this->conn = null;
 
         try {
-          $this->$conn = new PDO("mysql:host=$this->host;dbname=$this->dbname", $username, $password);
+            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
 
-          $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch(PDOException $e) {
-          echo 'Connection error: '. $e->getMessage();
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo 'Connection error: '. $e->getMessage();
         }
 
-        return $this->conn;
-      }
+          return $this->conn;
     }
-?>
+}
